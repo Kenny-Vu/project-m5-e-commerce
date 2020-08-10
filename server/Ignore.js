0a8db66c56f906e98a19b03b852c16784a06217d
Kenny Vu <kennyvu@live.ca>
2020-08-09 20:05:04 -0400

    Added a  POST endpoint and PUT endpoint

--- server/Ignore.js
+++ server/Ignore.js
@@ -1,11 +1,67 @@
 //This file will have snippets of code that shouldn't be implemented yet and should be ignored when staging changes
-
-//To follow REST principles, we will have to use a PUT method to update the item quantities in items.json
 const handleUpdateItemsData = (req, res) => {
-  const { itemIds, itemsQuantity } = req.body;
-  const itemsToUpdate = findItems(itemIds);
-  itemsToUpdate.forEach((item) => {
-    //loop through itemsQuantity array?
+  const { orderContent } = req.body;
+  const rejectedItems = [];
+  const approvedItems = [];
+  //First check
+  const allClear = orderContent.every((element) => {
+    items.some((item) => {
+      return item.id === element.id && item.numInStock > element.quantity;
+    });
   });
-  res.status(200).json(items[0]);
+
+  if (allClear) {
+    items.forEach((item) => {
+      orderContent.forEach((itemToUpdate) => {
+        if (item.id === itemToUpdate.id) {
+          item.numInStock -= itemToUpdate.quantity;
+          approvedItems.push({
+            itemId: item.id,
+            newAmountInStock: item.numInStock,
+            amountOrdered: itemToUpdate.quantity,
+          });
+        }
+      });
+    });
+  } else {
+    items.forEach((item) => {
+      orderContent.forEach((itemToUpdate) => {
+        if (item.id === itemToUpdate.id) {
+          rejectedItems.push({
+            itemId: item.id,
+            newAmountInStock: item.numInStock,
+            amountOrdered: itemToUpdate.quantity,
+          });
+        }
+      });
+    });
+  }
 };
+
+//OLD HANDLEUPDATEITEMSDATA
+// const handleUpdateItemsData = (req, res) => {
+//     const { orderContent } = req.body;
+//     const rejectedItems = [];
+//     const approvedItems = [];
+//     //Check client's order and update storage quantities
+//     items.forEach((item) => {
+//       orderContent.forEach((itemToUpdate) => {
+//         if (itemToUpdate.id === item.id) {
+//           //if there's enough of this item in storage then update the quantity
+//           if (item.numInStock > itemToUpdate.quantity) {
+//             item.numInStock -= itemToUpdate.quantity;
+//             approvedItems.push({
+//               itemId: item.id,
+//               newAmountInStock: item.numInStock,
+//               amountOrdered: itemToUpdate.quantity,
+//             });
+//           } else {
+//             rejectedItems.push({
+//               itemId: item.id,
+//               numInStock: item.numInStock,
+//               amountOrdered: itemToUpdate.quantity,
+//             });
+//           }
+//         }
+//       });
+//     });
