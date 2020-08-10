95b2a9ab92ae6434db8ecd996d3c9fbbe19fa145
myriam mansouri <myriam.mansouri@gmail.com>
2020-08-10 10:48:16 -0400

    Created independant button component and added hover effect

---
+++ client/src/components/Button.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 100px;
+  outline: none;
+  border: none;
+  background-color: #4848e5d6;
+  box-shadow: 2px 2px 5px grey;
+  color: white;
+  border-radius: 5px;
+  text-shadow: 1px 1px 4px black;
+  cursor: pointer;
+  &:hover{
+      background-color:#4848e5
+  }
+`;
+
+export default Button;
