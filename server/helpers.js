fdbed57a853d8ee526dd42a35cb9e2ef3c9c00a8
Kenny Vu <kennyvu@live.ca>
2020-08-13 16:55:30 -0400

    Added comments to the helpers.js, spinner.js and ErroPage.js for more clarity

--- server/helpers.js
+++ server/helpers.js
@@ -7,6 +7,7 @@ const searchById = (id, array) => {
   return array.find((element) => element.id === Number(id));
 };
 
+//This helper simulates delays and errors to make the website feel more "real"
 const simulateDelays = (res, data, status) => {
   const delay = Math.random() * maxDelay;
   const error = errorRate >= Math.random();
