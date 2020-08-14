142ea70db343adb10f765d56524a6c35baafc2df
Kenny Vu <kennyvu@live.ca>
2020-08-13 23:43:34 -0400

    Set the errorRate to zero since our error simulator isn't working as intended   :(

--- server/helpers.js
+++ server/helpers.js
@@ -1,6 +1,6 @@
 // This file is for Helper functions here that could potentially be reused
 const maxDelay = 2000;
-const errorRate = 0.05;
+const errorRate = 0; // Our error simulator in "simulateDelays" isn't working as intended so we'll at least make sure there are no errors
 
 //function to find specific item or company by passing the id and the array of objects as arguments(e.g. --> searchById(item.id,items))
 const searchById = (id, array) => {
@@ -13,7 +13,8 @@ const simulateDelays = (res, data, status) => {
   const error = errorRate >= Math.random();
   setTimeout(() => {
     if (error) {
-      throw new Error(error);
+      throw new Error(error); //This option somehow keeps the error and refreshing the page doesn't remove the error no matter what the error odds are
+      // res.status(500); <-- this option is never caught in the fetch from the FE... so we see the loading screen forever
     } else res.status(status).json(data);
   }, delay);
 };
